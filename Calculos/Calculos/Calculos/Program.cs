795f250b78c9777989bf8b78a981acc00a00e772
Marcos Laviola <marcoshenriquelaviola@gmail.com>
2026-08-27 22:10:59 -0300

    Exercicio para calcular a area de um trapezio

---
+++ Calculos/Calculos/Calculos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos {
+    class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            //Programa para calcular a area
+            double b1, b2, h, area;
+
+            Console.WriteLine("escolha um numero para base 1: ");
+            b1 = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("escolha um numero para base 2: ");
+            b2 = double.Parse(Console.ReadLine(), CI);
+
+            Console.WriteLine("escolha um numero para altura: ");
+            h = double.Parse(Console.ReadLine(), CI);
+
+            area = (b1 + b2) * h / 2.0;
+
+            Console.WriteLine("a area do trapezio é: " + area.ToString("F2", CI));
+        }
+    }
+}
